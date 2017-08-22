5c38f15cbf7b4676f075b8219d8db9e9d5c3a7dd
mell_hime <mell_hime@mail.ru>
2017-08-22 11:59:39 +0300

    some changes

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Флэшкарточкер'" do
+      visit '/'
+      expect(page).to have_content('Флэшкарточкер')
+    end
+  end
+end
