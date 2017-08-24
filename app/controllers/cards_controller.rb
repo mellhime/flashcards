8699e39a57fca560b31d78905298dab366505a06
mell_hime <mell_hime@mail.ru>
2017-08-24 20:06:16 +0300

    remove dublicates

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,12 @@
 class CardsController < ApplicationController
+  before_action :find_card
+  skip_before_action :find_card, except: [:show, :edit, :update, :destroy]
 
   def index
     @cards = Card.all
   end
 
   def show
-    @card = Card.find(params[:id])
   end
 
   def new
@@ -13,7 +14,6 @@ class CardsController < ApplicationController
   end
 
   def edit
-    @card = Card.find(params[:id])
   end
 
   def create
@@ -26,7 +26,6 @@ class CardsController < ApplicationController
   end
 
   def update
-    @card = Card.find(params[:id])
     if @card.update_attributes(card_params)
       redirect_to @card
     else
@@ -35,7 +34,7 @@ class CardsController < ApplicationController
   end
 
   def destroy
-    Card.find(params[:id]).destroy
+    @card.destroy
     redirect_to cards_path
   end
 
@@ -43,5 +42,9 @@ class CardsController < ApplicationController
 
   def card_params
     params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
+
+  def find_card
+    @card = Card.find(params[:id])
   end
 end
