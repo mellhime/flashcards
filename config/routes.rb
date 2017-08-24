7f1c8593dff4d06a9b5a642d54113e9c7d5fb894
mell_hime <mell_hime@mail.ru>
2017-08-24 16:14:24 +0300

    corrections

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   resources :cards
   root 'static_pages#home'
-  
   match '/add', to: 'cards#new', via: 'get'
-  #match '/edit',      to: 'cards#edit',           via: 'get'
-  # match '/cards/new', to: 'cards#create', via: 'get'
-  #match '/edit', to: 'cards#edit', via: 'get'
 end
