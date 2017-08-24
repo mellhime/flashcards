3d06c5e120eea65e0e174f136dacca7f4b4007d5
mell_hime <mell_hime@mail.ru>
2017-08-24 18:51:26 +0300

    some changes

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -41,7 +41,7 @@ class CardsController < ApplicationController
 
   private
 
-    def card_params
-      params.require(:card).permit(:original_text, :translated_text, :review_date)
-    end
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
 end
