71623814a70b1bcbb0dff6a5054aaada0421f796
mell_hime <mell_hime@mail.ru>
2017-08-27 14:35:20 +0300

    add seeds.rb script for db populating

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,9 @@
 class Card < ApplicationRecord
   before_validation :create_review_date, on: :create
   VALID_ORIGINAL_TEXT_REGEX = /[A-Za-z]/
-  validates :original_text, presence: true, length: { maximum: 35 }, format: { with: VALID_ORIGINAL_TEXT_REGEX }
+  validates :original_text, presence: true#, length: { maximum: 35 }, format: { with: VALID_ORIGINAL_TEXT_REGEX }
   VALID_TRANSLATED_TEXT_REGEX = /[а-яА-ЯёЁ]/
-  validates :translated_text, presence: true, length: { maximum: 35 }, format: { with: VALID_TRANSLATED_TEXT_REGEX }
+  validates :translated_text, presence: true#, length: { maximum: 35 }, format: { with: VALID_TRANSLATED_TEXT_REGEX }
   validate :equality_of_original_and_translated_texts
   validates :review_date, presence: true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+
+url = 'http://www.en365.ru/top100.htm'
+html = open(url)
+doc = Nokogiri::HTML(html, "UTF-8")
+original_texts = []
+translated_texts = []
+
+doc.css('tr').each do |tr|
+  unless tr.css('td')[1].nil?
+    original_texts << tr.css('td')[1].text 
+    translated_texts << tr.css('td')[2].text
+  end
+end
+
+original_texts = original_texts.slice(2..-1).map { |word| word.slice(0..(word.index(' ')))}
+translated_texts = translated_texts.slice(2..-1)
+new_arr = original_texts.zip(translated_texts)
+
+new_arr.each do |pair|
+  Card.create(original_text: pair[0], translated_text: pair[1])
+end
