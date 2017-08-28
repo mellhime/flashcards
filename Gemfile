2e6244a050aaaae162f88c785476bfbaa34ac0d0
mell_hime <mell_hime@mail.ru>
2017-08-28 14:37:37 +0300

    separate the parser from seeds.rb

--- Gemfile
+++ Gemfile
@@ -27,7 +27,6 @@ gem 'jquery-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
-gem 'nokogiri'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
@@ -55,6 +54,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'nokogiri'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ lib/my_parser.rb
@@ -0,0 +1,25 @@
+class MyParser
+
+  require 'open-uri'
+  require 'nokogiri'
+
+  def self.perform
+    url = 'http://www.en365.ru/top100.htm'
+    html = open(url)
+    doc = Nokogiri::HTML(html, "UTF-8")
+    original_texts = []
+    translated_texts = []
+
+    doc.css('tr').each do |tr|
+      unless tr.css('td')[1].nil?
+        original_texts << tr.css('td')[1].text 
+        translated_texts << tr.css('td')[2].text
+      end
+    end
+
+    original_texts = original_texts.slice(2..-1).map { |word| word.slice(0..(word.index(' ')))}
+    translated_texts = translated_texts.slice(2..-1)
+    new_arr = original_texts.zip(translated_texts)
+    new_arr
+  end
+end
