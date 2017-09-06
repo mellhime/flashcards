c2c11a903d33ba69282673132a4aa142a92bdcd4
mell_hime <mell_hime@mail.ru>
2017-09-06 20:28:49 +0300

    add associations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,5 @@
 class Card < ApplicationRecord
+  belongs_to :user
   before_validation :create_review_date, on: :create
   VALID_ORIGINAL_TEXT_REGEX = /\A[A-z]+\z/
   validates :original_text, presence: true, length: { maximum: 35 }, format: { with: VALID_ORIGINAL_TEXT_REGEX }
@@ -6,6 +7,7 @@ class Card < ApplicationRecord
   validates :translated_text, presence: true, length: { maximum: 35 }, format: { with: VALID_TRANSLATED_TEXT_REGEX }
   validate :equality_of_original_and_translated_texts
   validates :review_date, presence: true
+  validates :user_id, presence: true
 
   def create_review_date
     self.review_date = Date.today + 3.days
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :cards
+  has_many :cards
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 30 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
