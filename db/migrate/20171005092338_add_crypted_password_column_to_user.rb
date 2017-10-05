00ccc57cbbb9119ec5ec1e32b77253d6a29d0f1b
mell_hime <mell_hime@mail.ru>
2017-10-05 13:13:17 +0300

    fix

--- db/migrate/20171005092338_add_crypted_password_column_to_user.rb
+++ db/migrate/20171005092338_add_crypted_password_column_to_user.rb
@@ -1,6 +1,10 @@
 class AddCryptedPasswordColumnToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :salt, :string
-    add_column :users, :crypted_password, :string
+    change_table :users do |t|
+      t.string :salt
+      t.string :crypted_password
+    end
+
+    remove_column :users, :password_digest
   end
 end
