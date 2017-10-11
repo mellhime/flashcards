a19fe12f64616e044647e08702061b648551b03d
mell_hime <mell_hime@mail.ru>
2017-10-11 12:17:11 +0300

    add send mails ability

---
+++ app/mailers/cards_mailer.rb
@@ -0,0 +1,9 @@
+class CardsMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+ 
+  def unreviewed_cards_email(user)
+    @user = user
+    @url  = 'http://localhost:3000/'
+    mail(to: @user.email, subject: 'You have cards to review')
+  end
+end
---
+++ spec/mailers/cards_mailer_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe CardsMailer, :type => :mailer do
+  describe "unreviewed_cards_email" do
+    let(:user) { create(:user) }
+    let(:mail) { CardsMailer.unreviewed_cards_email(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("You have cards to review")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["notifications@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Welcome")
+    end
+  end
+end
