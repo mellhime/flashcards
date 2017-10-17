10659bc40ad85342133dbb6cf9c9de006db3ade5
mell_hime <mell_hime@mail.ru>
2017-10-17 15:50:12 +0300

    modify js script

--- app/interactors/check_card.rb
+++ app/interactors/check_card.rb
@@ -1,11 +1,11 @@
 class CheckCard
   include Interactor
 
-  SCORE = { 1..15 => 5, 16..60 => 4, 61..Float::INFINITY => 3 }
+  SCORE = { 1..15 => 5, 16..60 => 4, 61..Float::INFINITY => 3 }.freeze
 
   def call
     if context.user_text == context.card.original_text
-      score = SCORE.select { |k,v| k.include?(context.seconds.to_i) }.values.first
+      score = SCORE.select { |k, _v| k.include?(context.seconds.to_i) }.values.first
       context.card.review_status += 1
       context.card.easiness_factor += (0.1 - (5 - score) * (0.08 + (5 - score) * 0.02))
       context.message = I18n.t('cards.check.success')
@@ -24,7 +24,7 @@ class CheckCard
                             else
                               context.card.interval * context.card.easiness_factor
                             end
-                            
+
     context.card.review_date = Time.current + context.card.interval.days
     context.card.save
   end
