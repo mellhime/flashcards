47f288cfeb888b3511ad6c7142e2c26ed2f7c111
mellhime <mell_hime@mail.ru>
2017-10-20 15:45:00 +0300

    try to fix timer

--- app/assets/javascripts/score.js
+++ app/assets/javascripts/score.js
@@ -5,3 +5,12 @@ function timer() {
   seconds += 1;
   document.getElementById('seconds').value = seconds;
 }
+
+$('#check-form').submit(function(e) {
+    e.preventDefault();
+    this.submit();
+
+    setTimeout(function(){
+        seconds = 0;
+    }, 100);
+});
